bedb48ae874ddae9fd1cff807bd54856d661541d
Santa claus <santa@northpole.com>
2020-03-31 00:17:21 +0300

    further addition of pipes

--- Diaries/src/app/diary/diary.component.ts
+++ Diaries/src/app/diary/diary.component.ts
@@ -9,11 +9,11 @@ import { Diary } from '../diary';
 export class DiaryComponent implements OnInit {
   goals:Diary[] = [
     new Diary(1, 'Waking up early in the morning', 'so as to attend the morning stand up in our respective groups',new Date(2020,3,14)),
-    new Diary(2,'attending morning standups','so as to be given instructions',new Date(2019,6,9)),
-    new Diary(3,'watching the television','so as to get the Corona update',new Date(2022,1,12)),
-    new Diary(4,'cooking breakfast','so as to gain energy to do the Ip',new Date(2019,0,18)),
-    new Diary(5,'covering the days content','so as to gain knowledge',new Date(2019,2,14)),
-    new Diary(6,'talking a walk','so as to relax my mind from the days work',new Date(2030,3,14)),
+    new Diary(2,'attending morning standups','so as to be given instructions',new Date(2020,4,9)),
+    new Diary(3,'watching the television','so as to get the Corona update',new Date(2017,5,12)),
+    new Diary(4,'cooking breakfast','so as to gain energy to do the Ip',new Date(2014,0,18)),
+    new Diary(5,'covering the days content','so as to gain knowledge',new Date(2016,5,14)),
+    new Diary(6,'talking a walk','so as to relax my mind from the days work',new Date(2016,3,14)),
     
     
   ];
