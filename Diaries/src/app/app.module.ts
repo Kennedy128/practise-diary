53b16c4d0b42913867b80113fd89dc6abd7da473
Santa claus <santa@northpole.com>
2020-03-31 00:29:44 +0300

    creation of pipes

--- Diaries/src/app/app.module.ts
+++ Diaries/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { DiaryComponent } from './diary/diary.component';
 import { DiaryDetailComponent } from './diary-detail/diary-detail.component';
 import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiaryComponent,
     DiaryDetailComponent,
-    StrikethroughDirective
+    StrikethroughDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
